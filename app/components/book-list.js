5db8153c37604606bd5c5da8f11c8451cdf5f270
Damien White <damien.white@visoftinc.com>
2016-01-18 13:57:21 -0500

    Added sorting and filtering to the book-list component

--- app/components/book-list.js
+++ app/components/book-list.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   books: [],
   sortKeys: [],
-  sortedBooks: Ember.computed.sort('books', 'sortKeys')
+  filter: '',
+
+  sortedBooks: Ember.computed.sort('books', 'sortKeys'),
+
+  filteredBooks: Ember.computed.filter('sortedBooks', function(book) {
+    var title = book.get('title').toLowerCase();
+    return title.indexOf(this.get('filter')) !== -1;
+  })
 });
--- tests/integration/components/book-list-test.js
+++ tests/integration/components/book-list-test.js
@@ -42,4 +42,14 @@ test('it orders a list of books by title', function(assert){
   assert.equal(this.$('.book').text(), "Book 2Book 1");
 });
 
+test('it filters a list of books by title', function(assert) {
+  var book1 = FactoryGuy.make('book', { title: 'Book 1' });
+  var book2 = FactoryGuy.make('book', { title: 'Book 2' });
+
+  this.set('books', [book1, book2]);
+  this.set('filter', '1');
+  this.render(hbs`{{book-list books=books filter=filter}}`);
+
+  assert.equal(this.$('.book').text(), "Book 1");
+});
 
