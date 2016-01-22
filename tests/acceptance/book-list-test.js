079edb7c54decb94b9056e709c359dcc69f489d3
Damien White <damien.white@visoftinc.com>
2016-01-21 21:35:34 -0500

    Added filtering UI to the book-list component

--- tests/acceptance/book-list-test.js
+++ tests/acceptance/book-list-test.js
@@ -21,3 +21,15 @@ test('should show list of books', function(assert) {
     assert.equal(find('.book').length, 3);
   });
 });
+
+test('should filter a list of books', function(assert) {
+  TestHelper.handleFindAll('book', {title: 'Book 1'}, {title: 'Book 2'});
+  visit('/books');
+  fillIn('#filter', '2');
+
+  andThen(function() {
+    assert.equal(find('.book').length, 1);
+    assert.equal(find('.book').data('title'), 'Book 2');
+  });
+});
+
