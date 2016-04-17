d975c66df219e8f5f2f75b9960172d9ac8a11ad7
Damien White <damien.white@visoftinc.com>
2016-04-17 12:51:51 -0400

    Added new factories for our other models

---
+++ tests/factories/author.js
@@ -0,0 +1,7 @@
+import FactoryGuy from 'ember-data-factory-guy';
+
+FactoryGuy.define('author', {
+  default: {
+    name: 'Damien White'
+  }
+});
---
+++ tests/factories/publisher.js
@@ -0,0 +1,7 @@
+import FactoryGuy from 'ember-data-factory-guy';
+
+FactoryGuy.define('publisher', {
+  default: {
+    name: 'Acme, Inc.'
+  }
+});
